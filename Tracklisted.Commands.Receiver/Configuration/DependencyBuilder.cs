4b50e1ef2710d0815b0f2e17cb77313bae0ab453
Michal Sakwa <saka1988@gmail.com>
2018-04-27 00:55:21 +0200

    Added DependencyInjection builder.

---
+++ Tracklisted.Commands.Receiver/Configuration/DependencyBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Tracklisted.Configuration;
+
+namespace Tracklisted.Commands.Receiver.Configuration
+{
+    public class DependencyBuilder
+    {
+        public static IServiceProvider Build()
+        {
+            var configuration = BuildConfiguration();
+
+            var services = new ServiceCollection()
+                .AddSingleton(configuration)
+                .RegisterConfiguration();
+
+            return services.
+                BuildServiceProvider();
+        }
+
+        public static IConfiguration BuildConfiguration()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", false)
+                .AddJsonFile($"appsettings.{environment}.json", true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+    }
+}
