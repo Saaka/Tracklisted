eb128546319fc361b82bdc0a6c022ae22fb4ad05
Michal Sakwa <saka1988@gmail.com>
2018-04-27 00:16:31 +0200

    Added command receiver.

---
+++ Tracklisted.Commands.Receiver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Tracklisted.Commands.Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan sleepTime = TimeSpan.FromSeconds(10);
+            int counter = 1;
+            while (true)
+            {
+                Console.WriteLine($"{DateTime.UtcNow.ToString()}: Message #{counter}");
+
+                Thread.Sleep(sleepTime);
+                counter++;
+            }
+        }
+    }
+}
