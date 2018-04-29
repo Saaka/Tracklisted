bd92dde74596b17f5325580aba3bf2f288a64c68
Michal Sakwa <saka1988@gmail.com>
2018-04-29 13:09:01 +0200

    Added registration of lastfm api client (not implemented yet).

---
+++ Tracklisted.Integration.Lastfm/Configuration/LastFmModuleRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tracklisted.Infrastructure.Configuration;
+
+namespace Tracklisted.Integration.Lastfm.Configuration
+{
+    public static class LastFmModuleRegistration
+    {
+        public static IServiceCollection RegisterLastfmModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .RegisterHttpClient<LastFmApiClient>();
+
+            return services;
+        }
+    }
+}
---
+++ Tracklisted.Integration.Lastfm/LastFmApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tracklisted.Integration.Lastfm
+{
+    public class LastFmApiClient
+    {
+    }
+}
